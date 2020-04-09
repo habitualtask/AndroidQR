400e1807735ff6694fa6dfc776311207fcb74c0a
song2 <msms9957@gmail.com>
2020-04-09 23:33:50 +0900

    2020-04-09

---
+++ app/src/main/java/com/example/androidqr/QrActivity.java
@@ -0,0 +1,28 @@
+package com.example.androidqr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class QrActivity extends AppCompatActivity {
+
+    private Button buttonStart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr);
+        buttonStart = (Button) findViewById(R.id.startbtn);
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
